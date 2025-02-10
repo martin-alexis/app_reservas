9139afc14eb37482c9be98fb6be8b456d438c3f1
martin-alexis <alexismartin090@gmail.com>
2025-02-10 10:38:13 -0300

    Se configuró para migrar la bdd a Turso correctamente

--- api/config.py
+++ api/config.py
@@ -1,14 +1,11 @@
-import json
-
 from dotenv import load_dotenv
 import os
+
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Configuraciones de Flask y SQLAlchemy
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    DATABASE_TOKEN = os.getenv('DATABASE_TOKEN')
+    SQLALCHEMY_DATABASE_URI = f"sqlite+{os.getenv('TURSO_DATABASE_URI')}/?authToken={os.getenv('TURSO_DATABASE_TOKEN')}&secure=true"
     SECRET_KEY = os.getenv('SECRET_KEY')
