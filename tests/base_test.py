a8e853d09c6b10ab4f2cb27610bef9314daae212
martin-alexis <alexismartin090@gmail.com>
2025-02-15 14:56:52 -0300

    se creo la configuracion base para ejecutar los tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,16 +1,43 @@
-# tests/base_test.py
 import unittest
-from your_app import app, db
+from api.app import create_app
+from api.app import db
+from api.app.models.users.tipos_usuarios_model import TiposUsuario, Tipo as TipoUsuarios
+from api.app.models.users.roles_model import Roles, TipoRoles
+from api.app.models.services.disponibilidad_servicios_model import DisponibilidadServicio, Estado
+from api.app.models.services.tipos_servicios_model import TiposServicio, Tipo as TipoServicios
+from api.app.models.reservas.estados_reserva_model import EstadoReserva, EstadosReserva
+from api.app.models.pagos.estados_pago_model import EstadosPago, TiposEstadoPago
+
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         """ Se ejecuta antes de cada prueba """
-        self.app_context = app.app_context()
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()  # Crea las tablas en la base de datos en memoria
+
+        # Insertar datos obligatorios en la base de datos
+        self.insertar_datos_base(TipoUsuarios, TiposUsuario, db.session, 'tipo')
+        self.insertar_datos_base(TipoRoles, Roles, db.session, 'tipo')
+        self.insertar_datos_base(TipoServicios, TiposServicio, db.session, 'tipo')
+        self.insertar_datos_base(Estado, DisponibilidadServicio, db.session, 'estado')
+        self.insertar_datos_base(EstadoReserva, EstadosReserva, db.session, 'estado')
+        self.insertar_datos_base(TiposEstadoPago, EstadosPago, db.session, 'estado')
+
+
+    def insertar_datos_base(self, clase_enum, modelo, db_session, campo):
+        for enum in clase_enum:
+            # Verifica si ya existe en la base de datos
+            if not modelo.query.filter_by(**{campo: enum.name}).first():
+                # Crea una nueva instancia del modelo y agrega el valor del Enum
+                instancia_modelo = modelo(**{campo: enum.value})
+                db_session.add(instancia_modelo)
+        db_session.commit()
 
     def tearDown(self):
         """ Se ejecuta después de cada prueba """
         db.session.remove()
-        db.drop_all()  # Borra todas las tablas
+        # db.drop_all()  # Borra todas las tablas
         self.app_context.pop()
