efb5cd530f832aa0b595ec576606413d60fb1916
martin-alexis <alexismartin090@gmail.com>
2025-02-22 17:27:02 -0300

    se elimino el tipo_usuario.to_json() porque daba error

--- api/app/models/users/usuarios_model.py
+++ api/app/models/users/usuarios_model.py
@@ -37,8 +37,7 @@ class Usuarios(db.Model):
             'correo': self.correo,
             'telefono': self.telefono,
             'imagen': self.imagen,
-            'tipos_usuario_id': self.tipos_usuario_id,
-            'tipo_usuario': self.tipo_usuario.to_json() if self.tipo_usuario else None
+            'tipos_usuario_id': self.tipos_usuario_id
         }
 
     @staticmethod
