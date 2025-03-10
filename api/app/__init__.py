2982c3fedff56ea84e83283b8461fe0e1b5b6fca
martin-alexis <alexismartin090@gmail.com>
2025-03-10 10:55:15 -0300

    se importo y se configuro la instancia de marshmallow

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from api.config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
-
+ma = Marshmallow()
 
 def create_app():
     app = Flask(__name__,
@@ -15,6 +16,7 @@ def create_app():
 
     Config.select_config(app)
     db.init_app(app)
+    ma.init_app(app)
 
 
     from .routes.usuarios_route import user_bp
