8991182903057952d9473784a07cdc25af011ffc
martin-alexis <alexismartin090@gmail.com>
2025-03-14 10:51:13 -0300

    se registro el blueprint paralas rutas v2

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -18,30 +18,10 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
 
-
-    # from api.app.v1.routes.usuarios_route import user_bp
-    # app.register_blueprint(user_bp, url_prefix='/')
-    #
-    # from api.app.v1.routes.auth_route import auth_bp
-    # app.register_blueprint(auth_bp, url_prefix='/')
-    #
-    # from api.app.v1.routes import services_bp
-    # app.register_blueprint(services_bp, url_prefix='/')
-    #
-    # from api.app.v1.routes import reservas_bp
-    # app.register_blueprint(reservas_bp, url_prefix='/')
-    #
-    # from api.app.v1.routes import valoraciones_bp
-    # app.register_blueprint(valoraciones_bp, url_prefix='/')
-    #
-    # from api.app.v1.routes import pagos_bp
-    # app.register_blueprint(pagos_bp, url_prefix='/')
-
-    # from api.v1_1 import api as api_v1_1
-    # from api.v2 import api as api_v2
     from api.app.v1 import api as api_v1
     app.register_blueprint(api_v1, url_prefix='/api/v1.0')
 
-    # app.register_blueprint(api_v1_1, url_prefix='/v1.1')
-    # app.register_blueprint(api_v2, url_prefix='/v2')
+    from api.app.v2 import api as api_v2
+    app.register_blueprint(api_v2, url_prefix='/api/v2.0')
+
     return app
