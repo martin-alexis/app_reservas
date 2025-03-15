ec88fac198deb7a227258d3d1d547247851c19f9
martin-alexis <alexismartin090@gmail.com>
2025-03-15 16:14:45 -0300

    se llama a la funcion obtener servicio por id

--- api/app/v2/routes/servicios_route.py
+++ api/app/v2/routes/servicios_route.py
@@ -5,5 +5,4 @@ from api.app.utils import common_routes as common
 
 @api.route('/servicios/<int:id_servicio>', methods=['GET'])
 def obtener_servicio_id (id_servicio):
-       return jsonify('hola')
-       # return common.obtener_servicio_por_id(id_servicio)
+       return common.obtener_servicio_por_id(id_servicio)
