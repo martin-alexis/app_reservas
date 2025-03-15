fc5c5ceaa2814c77362dad3e7a78d3703c21f363
martin-alexis <alexismartin090@gmail.com>
2025-03-15 11:49:26 -0300

    se limino schemas y se comento common routes

--- api/app/utils/common_routes.py
+++ api/app/utils/common_routes.py
@@ -1,14 +1,14 @@
 
-from api.app.models.services.servicios_model import Servicios
-from api.app.schemas.servicios.schema_servicios import servicio_schema
-from api.app.utils.responses import APIResponse
-
-
-def obtener_servicio_por_id(id_servicio):
-    try:
-        servicio = Servicios.query.get(id_servicio)
-        if not servicio:
-            return APIResponse.not_found(None,'Servicios')
-        return APIResponse.success(servicio_schema.dump(servicio), 'Servicio encontrado')
-    except Exception as e:
-        return APIResponse.error(None, str(e))
+# from api.app.models.services.servicios_model import Servicios
+# from api.app.schemas.servicios.schema_servicios import servicio_schema
+# from api.app.utils.responses import APIResponse
+#
+#
+# def obtener_servicio_por_id(id_servicio):
+#     try:
+#         servicio = Servicios.query.get(id_servicio)
+#         if not servicio:
+#             return APIResponse.not_found(None,'Servicios')
+#         return APIResponse.success(servicio_schema.dump(servicio), 'Servicio encontrado')
+#     except Exception as e:
+#         return APIResponse.error(None, str(e))
