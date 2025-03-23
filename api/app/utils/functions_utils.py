a4f2d19b2a9a1ca8e2932be832abed67db238fdc
martin-alexis <alexismartin090@gmail.com>
2025-03-23 10:06:18 -0300

    se cambio el parametro de las funciones. en lugar de pasar el objeto user, aho se pasa solo el id del usuario. Ademas, se comento la funcion get_usertype ya que por ahora no ha sido utilizada

--- api/app/utils/functions_utils.py
+++ api/app/utils/functions_utils.py
@@ -4,23 +4,24 @@ from api.app.models.users.usuarios_tiene_roles_model import UsuariosTieneRoles
 from api.app.utils.responses import APIResponse
 
 
-def get_roles_user(user):
+def get_roles_user(id_usuario):
     try:
         roles_user = []
-        user_role_relations = UsuariosTieneRoles.query.filter_by(usuarios_id=user.id_usuarios).all()
+        user_role_relations = UsuariosTieneRoles.query.filter_by(usuarios_id=id_usuario).all()
 
         for relation in user_role_relations:
             role = Roles.query.get(relation.roles_id)
             if role:
                 role_str = str(role.tipo).replace('TipoRoles.', '')
                 roles_user.append(role_str)
+                print(roles_user)
         return roles_user
     except Exception as e:
         return APIResponse.error(None, str(e))
 
-def get_usertype(user):
-    try:
-        type_user = TiposUsuario.query.filter_by(id_tipos_usuario=user.tipos_usuario_id).first()
-        return str(type_user.tipo).replace('Tipo.', '')
-    except Exception as e:
-        return APIResponse.error(None, str(e))
+# def get_usertype(id_usuario):
+#     try:
+#         type_user = TiposUsuario.query.filter_by(id_tipos_usuario=id_usuario.tipos_usuario_id).first()
+#         return str(type_user.tipo).replace('Tipo.', '')
+#     except Exception as e:
+#         return APIResponse.error(None, str(e))
