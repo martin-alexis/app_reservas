c4304d6adeaa31c44ab37d8c79d520126374aa47
martin-alexis <alexismartin090@gmail.com>
2025-03-23 15:25:55 -0300

    se actualizo las respuesta json not_found

--- api/app/utils/common_routes.py
+++ api/app/utils/common_routes.py
@@ -1,4 +1,3 @@
-
 from api.app.models.services.servicios_model import Servicios
 from api.app.schemas.servicios.schema_servicios import ServiciosSchema
 from api.app.utils.responses import APIResponse
@@ -8,7 +7,7 @@ def obtener_servicio_por_id(id_servicio):
     try:
         servicio = Servicios.query.get(id_servicio)
         if not servicio:
-            return APIResponse.not_found()
+            return APIResponse.not_found(resource='Servicio')
         servicio_schema = ServiciosSchema()
         return APIResponse.success(data=servicio_schema.dump(servicio))
     except Exception as e:
