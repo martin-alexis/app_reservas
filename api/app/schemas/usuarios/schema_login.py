603afbdedc5013da26269070cf846ae60eda0171
martin-alexis <alexismartin090@gmail.com>
2025-03-24 10:32:56 -0300

    se creo el schema para el login

---
+++ api/app/schemas/usuarios/schema_login.py
@@ -0,0 +1,22 @@
+from marshmallow import Schema, fields, validate
+
+class LoginSchema(Schema):
+    correo = fields.Email(
+        required=True,
+        validate=validate.Email(error="Correo inválido. Ingresa un correo válido en formato 'usuario@dominio.com'."),
+        error_messages={"required": "El correo es obligatorio."}
+    )
+
+    contrasena = fields.Str(
+        required=True,
+        validate=[
+            validate.Length(min=8, max=32, error="La contraseña debe tener entre 8 y 32 caracteres."),
+            validate.Regexp(
+                regex=r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]+$",
+                error="La contraseña debe contener al menos una letra y un número."
+            )
+        ],
+        error_messages={"required": "La contraseña es obligatoria."}
+    )
+
+login_schema = LoginSchema()
