07a5573d02579a6832fbeed7c05632447b86f80d
martin-alexis <alexismartin090@gmail.com>
2025-03-24 12:37:31 -0300

    se corrigio un error

--- api/app/v2/routes/login_route.py
+++ api/app/v2/routes/login_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
 
+from api.app.models.users.usuarios_model import Usuarios
 from api.app.schemas.usuarios.schema_login import LoginSchema
 from api.app.utils.functions_utils import obtener_usuario_por_correo, get_roles_user
 from api.app.utils.responses import APIResponse
@@ -23,22 +24,24 @@ def verificar_usuario(data):
 @api.route('/login', methods=['POST'])
 def login_jwt():
     try:
-
         login_schema = LoginSchema()
         login_data = login_schema.load(request.json)
 
         usuario_verificado = verificar_usuario(login_data)
 
+        # Si verificar_usuario devuelve una respuesta de error, la retornamos directamente
+        if not isinstance(usuario_verificado, Usuarios):
+            return usuario_verificado
+
         roles_user = get_roles_user(usuario_verificado.id_usuarios)
 
         jwt_token = Security.create_token(usuario_verificado.id_usuarios, usuario_verificado.correo, roles_user)
 
-        return APIResponse.success(data= {'jwt_token': jwt_token})
+        return APIResponse.success(data={'jwt_token': jwt_token})
 
     except ValidationError as err:
         return APIResponse.validation_error(errors=err.messages)
 
     except Exception as e:
         return APIResponse.error(error=str(e))
-
 
