b9f75f37626961c055dda49801d34f640dbf69f8
martin-alexis <alexismartin090@gmail.com>
2025-03-26 13:02:20 -0300

    se paso la funcion subir_imagen_cloudinary aca

--- api/app/utils/functions_utils.py
+++ api/app/utils/functions_utils.py
@@ -1,5 +1,7 @@
+from werkzeug.utils import secure_filename
+import cloudinary.uploader
+
 from api.app.models.users.roles_model import Roles
-from api.app.models.users.tipos_usuarios_model import TiposUsuario
 from api.app.models.users.usuarios_model import Usuarios
 from api.app.models.users.usuarios_tiene_roles_model import UsuariosTieneRoles
 from api.app.utils.responses import APIResponse
@@ -33,3 +35,25 @@ def obtener_usuario_por_correo(correo):
             return usuario
         else:
             return None
+
+
+def subir_imagen_cloudinary(imagen, id_usuario_token, modelo):
+    try:
+
+        if not imagen:
+            return None
+
+        imagen_filename = secure_filename(imagen.filename)
+        carpeta_principal = f'app_reservas/{modelo}'
+        carpeta_usuario = f"id_{id_usuario_token}"
+        carpeta_completa = f"{carpeta_principal}/{carpeta_usuario}"
+
+        upload_result = cloudinary.uploader.upload(
+            imagen,
+            folder=carpeta_completa,
+            public_id=imagen_filename
+        )
+        return upload_result['secure_url']
+
+    except Exception as e:
+        return APIResponse.error(None, error=str(e), code=500, message='Error al subir la imagen a Cloudinary')
