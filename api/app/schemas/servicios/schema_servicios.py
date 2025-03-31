7cd92683eb63b18bceecb9b76d6fc88017e10087
martin-alexis <alexismartin090@gmail.com>
2025-03-31 13:34:25 -0300

    Se agrego una funcion post_load que pasa decimal a float el campo precio y ademas se agrego campos de entrada y salida en el modelo servicios

--- api/app/schemas/servicios/schema_servicios.py
+++ api/app/schemas/servicios/schema_servicios.py
@@ -1,6 +1,10 @@
-from marshmallow import post_load
+from decimal import Decimal
+
+from marshmallow import post_load, fields, validate
 from api.app import ma
+from api.app.models.services.disponibilidad_servicios_model import Estado
 from api.app.models.services.servicios_model import Servicios
+from api.app.models.services.tipos_servicios_model import Tipo
 
 from api.app.schemas.servicios.schema_tipos_servicios import TiposServicioSchema
 from api.app.schemas.usuarios.schema_usuarios import UsuariosSchema
@@ -19,23 +23,43 @@ class ServiciosSchema(ma.SQLAlchemySchema):
     precio = ma.auto_field()
     ubicacion = ma.auto_field()
     imagen = ma.auto_field()
+
+    tipos_servicio = fields.String(required=True, load_only=True, validate=validate.OneOf([tipo.value for tipo in Tipo],
+                                                                                         error="Tipo de servicio inválido."))
+    disponibilidad_servicio = fields.String(required=True, load_only=True, validate=validate.OneOf([estado.value for estado in Estado], error="Disponibilidad de servicio inválida."))
+
     # tipos_servicio_id = ma.auto_field()
-    # usuarios_proveedores_id = ma.auto_field()
+    usuarios_proveedores_id = ma.auto_field(dump_only=True)
     # disponibilidad_servicio_id = ma.auto_field()
 
-    tipo_servicio = ma.Nested(TiposServicioSchema)
-    # proveedor = ma.Nested(UsuariosSchema)
-    disponibilidad = ma.Nested(DisponibilidadServicioSchema)
-    # reservas = ma.List(ma.Nested(ReservasSchema))
-    usuarios_proveedores_id = ma.auto_field()
+    tipo_servicio = fields.Nested(TiposServicioSchema, dump_only=True)
+    proveedor = fields.Nested(UsuariosSchema, dump_only=True)
+    disponibilidad = fields.Nested(DisponibilidadServicioSchema, dump_only=True)
+
+    # tipo_servicio = fields.String(required=True, load_only=True, validate=validate.OneOf([tipo.value for tipo in Tipo],error="Tipo de servicio inválido."))
+    # disponibilidad_servicio = fields.String(required=True, load_only=True, validate=validate.OneOf([estado.value for estado in Estado], error="Disponibilidad de servicio inválida."))
+    # # tipos_servicio_id = ma.auto_field()
+    # # usuarios_proveedores_id = ma.auto_field()
+    # # disponibilidad_servicio_id = ma.auto_field()
+    #
+    # tipo_servicio = ma.Nested(TiposServicioSchema, dump_only=True)
+    # # proveedor = ma.Nested(UsuariosSchema)
+    # disponibilidad = ma.Nested(DisponibilidadServicioSchema, dump_only=True)
+    # # reservas = ma.List(ma.Nested(ReservasSchema))
+    # usuarios_proveedores_id = ma.auto_field()
 
     # valoraciones = ma.List(ma.Nested('ValoracionesSchema'))
 
+    # @post_load
+    # def make_servicio(self, data, **kwargs):
+    #     return Servicios(**data)
     @post_load
-    def make_servicio(self, data, **kwargs):
-        return Servicios(**data)
+    def convertir_decimal_a_float(self, data, **kwargs):
+        """Convierte precio de Decimal a float si es necesario."""
+        if isinstance(data.get("precio"), Decimal):
+            data["precio"] = float(data["precio"])
+        return data
 
-
-# Instancias del esquema para serialización
-# servicio_schema = ServiciosSchema()
-# servicios_schema = ServiciosSchema(many=True)
+#Instancias del esquema para serialización
+servicio_schema = ServiciosSchema()
+servicios_schema = ServiciosSchema(many=True)
