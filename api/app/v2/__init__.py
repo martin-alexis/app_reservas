749c5150d30fb7b380d62e0927be77a12c77dc5f
martin-alexis <alexismartin090@gmail.com>
2025-04-08 16:01:09 -0300

    se agregó la ruta de reservas al blueprint api_v2

--- api/app/v2/__init__.py
+++ api/app/v2/__init__.py
@@ -5,3 +5,4 @@ api = Blueprint('api_v2', __name__)
 from api.app.v2.routes import servicios_route
 from api.app.v2.routes import usuarios_route
 from api.app.v2.routes import login_route
+from api.app.v2.routes import reservas_route
