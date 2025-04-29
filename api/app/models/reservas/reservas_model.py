17c4ff2d34dddc4e7af1623ce99d19b2ce139cd5
martin-alexis <alexismartin090@gmail.com>
2025-04-29 15:43:48 -0300

    se elimino el campo monto_total del modelo reservas

--- api/app/models/reservas/reservas_model.py
+++ api/app/models/reservas/reservas_model.py
@@ -10,7 +10,6 @@ class Reservas(db.Model):
     fecha_creacion_reserva = db.Column(DateTime, default=datetime.utcnow, nullable=False)
     fecha_inicio_reserva = db.Column(DateTime, nullable=False)
     fecha_fin_reserva = db.Column(DateTime, nullable=False)
-    monto_total = db.Column(Numeric(10, 2), nullable=False)
     servicios_id = db.Column(Integer, ForeignKey('servicios.id_servicios'), nullable=False)
     estados_reserva_id = db.Column(Integer, ForeignKey('estados_reserva.id_estados_reserva'), nullable=False)
 
@@ -18,22 +17,11 @@ class Reservas(db.Model):
     estado_reserva = relationship('EstadosReserva', back_populates='reservas')
     pagos = relationship('Pagos', back_populates='reserva')
 
-    def __init__(self, fecha_inicio_reserva, fecha_fin_reserva, monto_total, servicios_id,
+    def __init__(self, fecha_inicio_reserva, fecha_fin_reserva, servicios_id,
                  estados_reserva_id):
         self.fecha_creacion_reserva = datetime.now()
         self.fecha_inicio_reserva = fecha_inicio_reserva
         self.fecha_fin_reserva = fecha_fin_reserva
-        self.monto_total = monto_total
         self.servicios_id = servicios_id
         self.estados_reserva_id = estados_reserva_id
 
-    def to_json(self):
-        return {
-            'id_reservas': self.id_reservas,
-            'fecha_creacion_reserva': self.fecha_creacion_reserva.isoformat(),
-            'fecha_inicio_reserva': self.fecha_inicio_reserva.isoformat(),
-            'fecha_fin_reserva': self.fecha_fin_reserva.isoformat(),
-            'monto_total': float(self.monto_total),
-            'servicios_id': self.servicios_id,
-            'estados_reserva_id': self.estados_reserva_id,
-        }
