4d1dc85ab4faeac9a8149a77de89efe363c1ab7b
Alexis Martin <alexismartin090@gmail.com>
2025-06-30 11:15:16 -0300

    se creo el import para el blueprint de pagos

--- api/app/blueprints_v2/__init__.py
+++ api/app/blueprints_v2/__init__.py
@@ -7,3 +7,4 @@ from api.app.usuarios.v2.routes import usuarios_route
 from api.app.login.v2.routes import login_route
 from api.app.reservas.v2.routes import reservas_route
 from api.app.preguntas.v2.routes import preguntas_route
+from api.app.pagos.v2.routes import pagos_route
