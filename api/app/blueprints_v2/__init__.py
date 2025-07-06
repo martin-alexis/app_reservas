33b4f3b4ca4276c2ec587be252e5e8f5d1711c92
Alexis Martin <alexismartin090@gmail.com>
2025-07-05 22:52:59 -0300

    Agrega la ruta para la autenticación de usuarios mediante Google en el módulo de login v2, integrando el nuevo endpoint para el inicio de sesión con Google.

--- api/app/blueprints_v2/__init__.py
+++ api/app/blueprints_v2/__init__.py
@@ -5,6 +5,7 @@ api = Blueprint('api_v2', __name__)
 from api.app.servicios.v2.routes import servicios_route
 from api.app.usuarios.v2.routes import usuarios_route
 from api.app.login.v2.routes import login_route
+from api.app.login.v2.routes import login_google_route
 from api.app.reservas.v2.routes import reservas_route
 from api.app.preguntas.v2.routes import preguntas_route
 from api.app.pagos.v2.routes import pagos_route
